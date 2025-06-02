80a6d17ac61bbf13143daa3566f976206d3ac7dc
Cyusa <asaphcyusa@hotmail.com>
2025-06-02 13:33:24 +0200

    key rotations

--- src/icutil_backend/src/key_rotation.rs
+++ src/icutil_backend/src/key_rotation.rs
@@ -2,7 +2,6 @@ struct KeyManager {
     current: Arc<Mutex<HashMap<u64, DeviceAuth>>>,
     pending_rotation: Arc<Mutex<HashMap<u64, (Vec<u8>, u32)>>>
 }
-
+// rotate keys every 24 hours
 async fn rotate_keys_heartbeat() {
-    // Automatically rotates keys every 24 hours
 }
